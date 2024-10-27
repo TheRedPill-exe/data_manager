47e636ab543d2862ccc95119687b5c62fd40403d
Pablo Marín <pablo.marin1@utp.edu.co>
2024-10-27 13:22:26 -0500

    updating the functions

--- src/file_handler.py
+++ src/file_handler.py
@@ -1,13 +1,35 @@
 import json
 
-with open("data/users.json","r") as jusers:
-    users = json.load(jusers)
+def load_users(filename="data/users.json"):
+    """Carga los datos de usuarios desde el archivo JSON."""
+    try:
+        with open(filename, "r") as jusers:
+            return json.load(jusers)
+    except (FileNotFoundError, json.JSONDecodeError):
+        print("Error al cargar el archivo JSON.")
+        return []
+    
+def save_update(users,filename="data/users.json"):
+    try:
+        with open(filename, "w") as jusers:
+            json.dump(users, jusers, indent=4)
+    except (FileNotFoundError, json.JSONDecodeError):
+        print("Error al sobrescribit los datos")
 
-def update_user(username, newusername):
+def find_data(field, data_to_find, users):
     for user in users:
-        if user.name == username:
-            user.name = newusername
-            break
+        if user.get(field) == data_to_find:
+            return True
+    return False
 
-    print("Usuario no encontrado")
-update_user("Juan","pablo")
+def update_username(field,new_data,filename="data/users.json"):
+    users = load_users()
+
+    try:
+        if(find_data(user_name)):
+            for user in users:
+                if user.get
+
+    
+
+
